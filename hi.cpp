b2dad99a8b1f05ffabec1b75c54c043d26e04aea
madhu <madhu@ev.com>
2022-10-01 17:57:25 +0530

    user interface update 2

--- hi.cpp
+++ hi.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout <<"nams "<< endl;
+cout <<"hi"<< endl;
 return 0;
 }
--- vanak.cpp
+++ vanak.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-cout <<"nams "<< endl;
+cout <<"vanakam"<< endl;
 return 0;
 }
